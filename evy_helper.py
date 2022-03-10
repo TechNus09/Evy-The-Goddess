886bb6104fd2299dfc882a9394487ee938d8e66d
TechNus09 <pegatech9@gmail.com>
2022-03-10 22:49:24 +0100

    cleanup

--- evy_helper.py
+++ evy_helper.py
@@ -1,29 +1,40 @@
 import json
-from db_helper import insert, retrieve
+#from db_helper import insert, retrieve
 from datetime import datetime 
 import time
+import math
+import copy
 
 
 def SortUp(old_log,new_log):
-    #sort sata from old and new records to give xp gains of each player
-    skills = ['mining_xp','woodcutting_xp']
+    #sort data from old and new records to give xp gains of each player
+    
+    skills = ['combat_xp','mining_xp','smithing_xp','woodcutting_xp','crafting_xp','fishing_xp','cooking_xp']
+    combat_unranked = {}
     mining_unranked = {}
+    smithing_unranked = {}
     wc_unranked = {}
+    crafting_unranked = {}
+    fishing_unranked = {}
+    cooking_unranked = {}
     total_unranked = {}
-    unranked = [mining_unranked,wc_unranked,total_unranked]
+    unranked = [combat_unranked,mining_unranked,smithing_unranked,wc_unranked,crafting_unranked,fishing_unranked,cooking_unranked,total_unranked]
 
-    for i in range(2):
+    for i in range(7):
         skill = skills[i]
         for j in new_log :
-            new_xp = new_log[j][skill]
-            old_xp = old_log[j][skill]
-            xp = new_xp - old_xp
-            unranked[i][j]=xp
-            if i == 0 :
-                unranked[2][j] = xp
+            if j in old_log :
+                new_xp = new_log[j][skill]
+                old_xp = old_log[j][skill]
+                xp = new_xp - old_xp
+                unranked[i][j]=xp
+                if i == 0 :
+                    unranked[7][j] = xp
+                else:
+                    unranked[7][j] += xp
             else:
-                unranked[2][j] += xp
-    return unranked
+                pass
+    return unranked #return list of dicts of unranked player:xp for ea skill
 
 def RankUp(unsortedlb):
     #make a rankings of players based on their xp gain
@@ -81,5 +92,303 @@ def get_tasks(session,skill_name):
         url='https://www.curseofaros.com/highscores'
         tasks.append(asyncio.create_task(session.get(url+skill_name+'.json?p='+str(k))))
     return tasks
+##############Embeds Helper#############
+def makeEmbeds(result,tag,skill):
+    members_count = len(result[0]) 
+    embeds_count = math.ceil(members_count/20)
+    total_xp = "{:,}".format(result[1])
+    for i in range(embeds_count):
+        
+        fields_list = []
+        for j in range(20):
+            rank = (i*20)+j+1
+            field = it.EmbedField(name=f"Rank#{rank}", value=result[0][rank-1])
+            fields_list.append(field)
+        embed = it.Embed(title="\u200b",
+        	                description="\u200b",       
+        	                fields=fields_list,
+        	                color=0x00ff00)
+        
+        embeds_list.append(embed)	   
+        main_embed = it.Embed(title=f"{tag}'s {skill} Leaderboard,
+        	                description=f"Members Count : {members_count}\nTotal Xp : {total_xp}",       
+        	                fields=[]
+        	                color=0x00ff00)        
+    return main_embed, embeds_list	                  
+            
+       
     
+    
+    
+
+        
+        
+def ToZero(dicc):
+    for key in dicc:
+        dicc[key]=0  
+    
+    
+    
+    
+def tabfill(xp): 
+    if xp>4536153492:
+        lvl=120
+        a=0
+    else :   
+        lvl=0
+        a=0
+        for l in range(120):
+            if (xp > lvltab[l]):
+                lvl = l+1
+                a = round((((xp- lvltab[l]) / lvldef[l])*100),2)
+    if a == 100:
+        a = 0
+        lvl += 1
+    return lvl, a
+
+def DictToList (dictio,listo):
+    listo.clear()
+    for key, value in dictio.items():
+        test = key + " -- " + "{:,}".format(value)
+        listo.append(test)
+
+def DictToList_alt (dictio):
+    temporal = []
+    for key, value in dictio.items():
+        test = key + " -- " + "{:,}".format(value)
+        temporal.append(test)
+    return temporal
+
+def ResetDict(diction):
+    diction = diction.fromkeys(diction, 0)
+    return diction
+
+def SortDict (di):
+    temp = {}
+    temp = {k: v for k, v in sorted(di.items(), key=lambda item: item[1],reverse=True)}
+    return temp
+
+def rankk (rank):
+    rank_text = "**rank#"+str(rank)+"**"
+    return rank_text
+
+#################xp getters###########
+async def makelog(g_tag) :
+    event_log = {}
+    name_list = []
+    c_skill = ["",'-mining', '-smithing', '-woodcutting', '-crafting', '-fishing', '-cooking']
+    c_xp = ['combat_xp','mining_xp','smithing_xp','woodcutting_xp','crafting_xp','fishing_xp','cooking_xp']
+
+    for skill_x in range(7):
+        #connector = aiohttp.TCPConnector(limit=80)
+        async with aiohttp.ClientSession() as session :
+            to_do = get_tasks(session, c_skill[skill_x])
+            responses = await asyncio.gather(*to_do)
+            for response in responses:
+                data = await response.json()
+                if data != []:
+                    for fdata in data :
+                        member_temp = { 'ign' : 'name' , 'combat_xp' : 0 , 'mining_xp' : 0 , 'smithing_xp' : 0 , 'woodcutting_xp': 0 , 'crafting_xp' : 0 , 'fishing_xp' : 0 , 'cooking_xp' : 0 , 'total': 0}
+                        player_name = fdata["name"]
+                        xp = fdata["xp"]
+                        tag = player_name.split()[0]                    
+                        if tag.upper() == g_tag :
+                            if player_name in name_list:
+                                event_log[player_name][c_xp[skill_x]]=xp
+                                event_log[player_name]["total"] += xp
+                            else:
+                                name_list.append(player_name)
+                                event_log[player_name]=member_temp
+                                event_log[player_name]["ign"] = player_name
+                                event_log[player_name][c_xp[skill_x]]=xp
+                                event_log[player_name]["total"] += xp
+                elif data == []:
+                    break
+
+    return event_log
+
+
+async def SearchEvent(skill_name):
+    global members_log, members_list, unsorted_lb
+    
+    
+
+    log_file = members_log
+    skills_list = skills_names_list
+    skills_xp = skills_xp_list
+    sorted_lb ={}
+    temp_dic = {}
+    members_sorted = []
+    unsortedl = {}
+    skill_x = skills_list.index(skill_name)
+    async with aiohttp.ClientSession() as session:
+        
+        to_do = get_tasks(session,skill[skill_x])
+        responses = await asyncio.gather(*to_do)
+        for response in responses:
+            data = await response.json()
+            for fdata in data:
+                player_name = fdata["name"]
+                xp = fdata["xp"]
+                tag = player_name.split()[0]
+                tag = tag.upper()
+                if player_name in namelist :
+                    name_order = namelist.index(player_name)
+                    old_xp = log_file[name_order][skills_xp[skill_x]]
+                    new_xp = xp
+                    xp_diff = new_xp - old_xp
+                    unsortedl[player_name] = xp_diff
+                    continue
+    temp_dic = {k: v for k, v in sorted(unsortedl.items(), key=lambda item: item[1],reverse=True)}
+    members_sorted.clear()
+    total_xp = 0
+    for key, value in temp_dic.items():
+        if value != 0 :
+            total_xp += value
+            test = key + " <> " + "{:,}".format(value)
+            members_sorted.append(test)
+        else:
+            continue
+    
+    mini_list = []
+    mini_list = members_sorted
+    temp_dic = {}
+    end = time.time()
+    total_time = math.ceil(end - start)
+    return mini_list, total_time, total_xp
+
+
+
+async def SearchEventTotal(old_log):
+   
+    log_file = members_log
+    skills_list = skills_names_list
+    skills_xp = skills_xp_list
+    sorted_lb ={}
+    temp_dic = {}
+    members_sorted = []
+    unsortedl = {}
+    for skill_x in range(7):
+        async with aiohttp.ClientSession() as session:
+            
+            to_do = get_tasks(session,skill[skill_x])
+            responses = await asyncio.gather(*to_do)
+            for response in responses:
+                data = await response.json()
+                if data != [] :
+                    for fdata in data :
+                        player_name = fdata["name"]
+                        xp = fdata["xp"]
+                        tag = player_name.split()[0]
+                        tag = tag.upper()
+                        if player_name in namelist :
+                            name_order = namelist.index(player_name)
+                            old_xp = log_file[name_order][skills_xp[skill_x]]
+                            new_xp = xp
+                            xp_diff = new_xp - old_xp
+                            if player_name in unsortedl:
+                                unsortedl[player_name] += xp_diff
+                            else:
+                                unsortedl[player_name] = xp_diff
+                            continue
+                elif data == [] :
+                    break
+    temp_dic = {k: v for k, v in sorted(unsortedl.items(), key=lambda item: item[1],reverse=True)}
+    members_sorted.clear()
+    total_xp = 0
+    for key, value in temp_dic.items():
+        if value != 0 :
+            total_xp += value
+            test = key + " <> " + "{:,}".format(value)
+            members_sorted.append(test)
+        else:
+            continue
+    mini_list = []
+    mini_list = members_sorted
+    return mini_list, total_xp
+
+
+##############################################################################
+#get guild members rankings in a certain skill (20000)    
+async def searchtag(skill_name,guildtag):
+    start = time.time()
+    members_sorted = []
+    guildreg_names = {}
+    guildreg_ranks = {}
+    async with aiohttp.ClientSession() as session:
+        to_do = get_tasks(session,skill_name)
+        responses = await asyncio.gather(*to_do)
+        for response in responses:
+            data = await response.json()
+            if data != []
+                for fdata in data: 
+                    player_name = fdata["name"]
+                    xp = fdata["xp"]
+                    tag = player_name.split()[0]
+                    tag = tag.upper()
+                    if tag == guildtag.upper():
+                        if player_name in guildreg_names :
+                            continue
+                        else:
+                            guildreg_names[player_name]=xp
+                            continue
+            elif data == [] :
+                break 
+    temp_dic = {k: v for k, v in sorted(guildreg_names.items(), key=lambda item: item[1],reverse=True)}
+    members_sorted.clear()
+    total_xp = 0
+    for key, value in temp_dic.items():
+        total_xp += value
+        test = key + " -- " + "{:,}".format(value) +"\n [Lv."+str(tabfill(value)[0])+" ("+str(tabfill(value)[1])+"%)]"
+        members_sorted.append(test)
+    mini_list = []
+    for i in range(len(members_sorted)):
+        mini_list.append(members_sorted[i])
+    members_sorted.clear()
+    temp_dic = {}
+    return mini_list, total_xp
+
+#get guilds members rankings in total xp (20000)
+async def searchtagtotal(guildtag):
+    
+    members_sorted = []
+    guildreg = {}
+    
+    for skill_name in skill :
+        async with aiohttp.ClientSession() as session:
+            to_do = get_tasks(session,skill_name)
+            responses = await asyncio.gather(*to_do)
+            for response in responses:
+                data = await response.json()
+                if data != [] :
+                    for fdata in data : 
+                        player_name = fdata["name"]
+                        xp = fdata["xp"]
+                        tag = player_name.split()[0]
+                        tag = tag.upper()
+                    
+                        if tag == guildtag.upper():
+                            if player_name in guildreg :
+                                guildreg[player_name]+=xp
+                                continue
+                            else:
+                                guildreg[player_name]=xp
+                                continue
+                elif data == [] :
+                    break
+    temp_dic = {k: v for k, v in sorted(guildreg.items(), key=lambda item: item[1],reverse=True)}
+    members_sorted.clear()
+    total_xp = 0
+    for key, value in temp_dic.items():
+        total_xp += value
+        test = key + " -- " + "{:,}".format(value)
+        members_sorted.append(test)
+    mini_list = []
+    for i in range(len(members_sorted)):
+        mini_list.append(members_sorted[i])
+    members_sorted.clear()
+    temp_dic = {}
+    return mini_list, total_xp
+
+
 
--- main.py
+++ main.py
@@ -7,8 +7,10 @@ import json
 import nest_asyncio
 import time
 import aiohttp
-
-from db_helper import *
+import interactions as it
+from interactions import Client, Button, ButtonStyle, SelectMenu, SelectOption, ActionRow
+from interactions import CommandContext as CC
+#from db_helper import *
 from evy_helper import *
 
    
@@ -20,40 +22,16 @@ event_log = {}
 #lock_state = True     
 
 
+skill_afx = ["",'-mining', '-smithing', '-woodcutting', '-crafting', '-fishing', '-cooking']
+skills = ['combat','mining', 'smithing', 'woodcutting', 'crafting', 'fishing', 'cooking']
 
-async def makelog() :
-    event_log = {}
-    start = time.time()
-    name_list = []
-    
-    c_xp = ['mining_xp','woodcutting_xp']
-    c_skill =['-mining', '-woodcutting']
-    
-    for skill_x in range(2):
-        #connector = aiohttp.TCPConnector(limit=80)
-        async with aiohttp.ClientSession() as session :
-            to_do = get_tasks(session, c_skill[skill_x])
-            responses = await asyncio.gather(*to_do)
-            for response in responses:
-                data = await response.json()
-                for fdata in data :
-                    member_temp = { 'ign' : 'name' , 'mining_xp' : 0 , 'woodcutting_xp': 0 , 'total': 0}
-                    player_name = fdata["name"]
-                    xp = fdata["xp"]
-                    tag = player_name.split()[0]                    
-                    if tag.upper() == "OWO":
-                        if player_name in name_list:
-                            event_log[player_name][c_xp[skill_x]]=xp
-                            event_log[player_name]["total"] += xp
-                        else:
-                            name_list.append(player_name)
-                            event_log[player_name]=member_temp
-                            event_log[player_name]["ign"] = player_name
-                            event_log[player_name][c_xp[skill_x]]=xp
-                            event_log[player_name]["total"] += xp
-    end = time.time()
-    total_time = math.ceil(end - start)
-    return event_log, total_time
+guilds_combat = {}
+guilds_mining = {}
+guilds_smithing = {}
+guilds_woodcutting = {}
+guilds_crafting = {}
+guilds_fishing = {}
+guilds_cooking = {}
 
 
 
@@ -61,16 +39,15 @@ async def makelog() :
 
 
 
-bot = Client(token=TOKEN)
 
-bot.remove_command("help")
-bot.remove_command("date")
-bot.remove_command('random')
+bot = Client(os.getenv("TOKEN"))
+
+
 @bot.event
 async def on_ready():
     #global lock_state
-    print('Logging in as {0.user}'.format(bot))
-
+    #print('Logging in as {0.user}'.format(bot))
+    print("Logged in !")
     #settings = retrieve('settings')
     #lock_state = settings['lock']
 
@@ -82,122 +59,67 @@ async def on_ready():
 
 
 
-@bot.command()
-async def log(ctx):
-    await ctx.send("logging members xp ... ")
-    if os.path.exists("data.json"):
-        os.remove("data.json")
-    
-    old_record = retrieve("0000")
-    create = crt(old_record)
-   
-    if create :
-        await ctx.send("logging finished \nsending log file ...")
-        await ctx.channel.send('collected data!', file=d.File("data.json"))
-    else:
-        await ctx.send("logging failed")
 
-
-
-
-@bot.command()
-async def create(ctx):
-    l = createT()
-    if l :
-        await ctx.send("table created")
-    else:
-        await ctx.send("error")
-
-
-
-#
-#@bot.command()
-#async def lock(ctx):
-#    global lock_state
-#    if lock_state :
-#        await ctx.send("Already Locked.")
-#    else:
-#        #change settings['lock'] to True
-#        s = {'lock':True}
-#        settings = jsing(s)
-#        update('setting',settings)
-#        await ctx.send('cmd "start" locked.')
-#
-#@bot.command()
-#async def unlock(ctx):
-#    global lock_state
-#    if lock_state :
-#        #change settings['lock'] to False
-#        s = {'lock':False}
-#        settings = jsing(s)
-#        update('setting',settings)
-#        await ctx.send('cmd "start" unlocked.')
-#    else:
-#        await ctx.send("Already Locked.")
-
-@bot.command()
-async def start(ctx):
-    msg1 = await ctx.send("Fetching records ...")
-    a = asyncio.run(makelog())
-    init_record = a[0] #dict object contain records
-    init_log = jsing(init_record) #json object contain records
-
-    await msg1.delete()
-    msg2 = await ctx.send("saving init records to DB ...")
-
-    await msg2.delete()
-    await update('0000',init_log,ctx)
-
-@bot.command()
-async def end(ctx):
-
-    a = asyncio.run(makelog())
-    final_record = a[0] #dict object contain records
-    final_log = jsing(final_record) #json object contain records
-
-    msg2 = await ctx.send("saving final records to DB ...")
-
-    msg2.delete()
-    await insert(ctx,'9999',final_log)
-
-@bot.command()
-async def event(ctx,skill='total'):
-    if skill.lower() in ['total','mining','woodcutting'] :
-        msg1 = await ctx.send("Fetching newest records")
-        old_record = retrieve("0000")
-        a = asyncio.run(makelog())
-        new_record = a[0]
-        unranked_data = SortUp(old_record,new_record)
-
-        if skill.lower() == 'total':
-            await msg1.delete()
-            ranked_data = RankUp(unranked_data[2])[0]
-            ranking = RankList(ranked_data)
-            oa_xp = RankUp(unranked_data[2])[1]
-            await ctx.send("Total Xp LeaderBoard")
-            await ctx.send(ranking)
-            await ctx.send("Overall Xp gain : " + "{:,}".format(oa_xp))
-            
-        elif skill.lower() == 'mining':
-            await msg1.delete()
-            ranked_data = RankUp(unranked_data[0])[0]
-            oa_xp = RankUp(unranked_data[0])[1]
-            ranking = RankList(ranked_data)
-            await ctx.send("Mining LeaderBoard")
-            await ctx.send(ranking)
-            await ctx.send("Overall Xp gain : " + "{:,}".format(oa_xp))
-            
-        elif skill.lower() == 'woodcutting':
-            await msg1.delete()
-            ranked_data = RankUp(unranked_data[1])[0]
-            oa_xp = RankUp(unranked_data[1])[1]
-            ranking = RankList(ranked_data)
-            await ctx.send("Woodcutting LeaderBoard")
-            await ctx.send(ranking)
-            await ctx.send("Overall Xp gain : " + "{:,}".format(oa_xp))
+  
+@bot.command(name="guildlb",
+             description="Show Guild's Leaderboard In Total Xp Or Specific Skill",
+             options=[
+                     	it.Option(
+                        		     name="Skill",
+                             	  description="The Leaderboard Skill",
+             		                type=it.OptionType.STRING,
+             		                required=True,
+             		                choices=[
+             			                       it.Choice(name="Total",value="total"),
+                	                       it.Choice(name="Combat",value="combat"),
+            	                           	it.Choice(name="Mining",value="mining"),
+           	                          		it.Choice(name="Smithinh",value="smithing"),
+             	                          	it.Choice(name="Woodcutting",value="woodcutting"),
+           	                           	it.Choice(name="Crafting",value="crafting"),              
+             	                          it.Choice(name="Fishing",value="fishing"),
+           	                           	it.Choice(name="Cooking",value="cooking"),
+             	                       			],
+              	              	),
+              	       it.Option(
+              	       	         name="tag",
+              	       	         description="Guild Tag To Look For",
+              	       	         type=it.OptionType.STRING,
+              	       	         required=False,
+              	       	         ),
+              	       	         
+             		      ],
+             			
+           	)        
+async def guildlb(ctx:CC,skill:str,tag:str="god"):
+    await ctx.defer()
+    g_tag = tag.upper()
+    if len(g_tag) > 5 or len(g_tag) < 2:
+        ctx.send("Invalid tag.\nValid tags length is between 2-5")
     else :
-        await ctx.send("Invalid Input ! \nPlease use one from : total - mining - woodcutting")
-
+    	await ctx.send("Fetching Data ...")
+        if skill == "total":
+            result = asyncio.run(searchtagtotal(g_tag)) 
+            embeds = makeEmbeds(result,g_tag,skill.capitalize())
+            e = embeds[1]
+            e.insert(0,embeds[0])
+        else :
+            skill_order = skills.index(skill)
+            result = asyncio.run(searchtag(skill_afx[skill_order],g_tag))
+            embeds = makeEmbeds(result,g_tag,skill.capitalize())
+            e = embeds[1]
+            e.insert(0,embeds[0])
+            
+        await ctx.edit("Guild Leaderboard",embeds=e)
+    
 
 
 bot.start()
+
+
+
+
+
+
+
+
+
