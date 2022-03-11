3b7dd73498a812a75a0d871905f7f8f3a214ff08
TechNus09 <59443272+TechNus09@users.noreply.github.com>
2022-03-11 06:38:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -155,17 +155,18 @@ async def guildlb(ctx:CC,skill:str,tag:str="god"):
         else :
             skill_order = skills.index(skill.lower())
             result = asyncio.run(searchtag(skill_afx[skill_order],g_tag))
-            print("fetching finished)
+            print("fetching finished")
             embeds = makeEmbeds(result,g_tag,skill.capitalize())
             print("embeds finished")
             ranking_embeds = embeds[1]
             main_embed = embeds[0]
-        print(ctx.author.user.username)
+        #print(ctx.author.user.username)
         user = ctx.author.user.username
-
         m_count = len(result[0])
         pager_reg[str(user)]=[0,m_count,rankings_embeds,main_embed]
         print(pager_reg)
+        pager_m = pagerMaker(0,m_count)
+        m_row = ActionRow(components=[pager_m])
         await ctx.edit("Finished !",embeds=[main_embed,embeds_list[0]],components=[m_row,b_row])
 
   
