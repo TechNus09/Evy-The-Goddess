722d2cbacde791af5ffcc5fd6074c5e8a7784947
TechNus09 <59443272+TechNus09@users.noreply.github.com>
2022-03-12 16:17:44 +0100

    response to the command invoker

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ import aiohttp
 import interactions as it
 from interactions import Client, Button, ButtonStyle, SelectMenu, SelectOption, ActionRow
 from interactions import CommandContext as CC
+from interactions import ComponentContext as CPC
 #from db_helper import *
 from evy_helper import *
 import logging
 
-   
+
 nest_asyncio.apply()
-     
- 
+
+
 event_log = {}
 pager_reg = {}
 #global lock_state
@@ -38,54 +39,36 @@ guilds_cooking = {}
 
 
 first_b = Button(
-                 style=ButtonStyle.PRIMARY, 
-                 label="⏪", 
-                 custom_id="first_button", )               
+                style=ButtonStyle.PRIMARY, 
+                label="⏪", 
+                custom_id="first_button", )               
 backward_b = Button(
-                 style=ButtonStyle.PRIMARY, 
-                 label="◀", 
-                 custom_id="backward_button", )
+                style=ButtonStyle.PRIMARY, 
+                label="◀", 
+                custom_id="backward_button", )
 stop_b = Button(
-                 style=ButtonStyle.DANGER, 
-                 label="◼",
-                 custom_id="stop_button", )
+                style=ButtonStyle.DANGER, 
+                label="◼",
+                custom_id="stop_button", )
 forward_b = Button(
-                 style=ButtonStyle.PRIMARY, 
-                 label="▶", 
-                 custom_id="forward_button", )
+                style=ButtonStyle.PRIMARY, 
+                label="▶", 
+                custom_id="forward_button", )
 last_b = Button(
-                 style=ButtonStyle.PRIMARY, 
-                 label="⏩", 
-                 custom_id="last_button", )
+                style=ButtonStyle.PRIMARY, 
+                label="⏩", 
+                custom_id="last_button", )
 b_row = ActionRow(
-                  components=[
-                              first_b,
-	                      backward_b,
-	                      stop_b,
-	                      forward_b,
-	                      last_b
-                             ]
-                 )
+                components=[
+                            first_b,
+	                        backward_b,
+	                        stop_b,
+	                        forward_b,
+	                        last_b
+                            ]
+                )
 
 
-def pagerMaker(pos,count):
-    options_list = []
-    leng = count // 20 + 1
-    for i in range(leng-1):
-        rrank = i*20 + 1
-        rankk = (i+1)*20 
-        option = SelectOption(label=f"Page {i+1} (#{rrank}--#{rankk})",
-                              value=str(i),
-                              )
-        options_list.append(option)
-    last_option = SelectOption(label=f"Page {leng} (#{(leng-1)*20+1}--#{count})",
-                                value=str(leng-1),
-                                )
-    options_list.append(last_option)
-    pager_menu = SelectMenu(options=options_list,
-	                           placeholder=f"Page ({pos+1}/{leng})",
-	                           custom_id="pager_menu", )
-    return pager_menu   
 
 
 
@@ -110,34 +93,34 @@ async def on_ready():
 
 
 
-  
+
 @bot.command(name="guildlb",
-             description="Show Guild's Leaderboard In Total Xp Or Specific Skill",
-             options=[
-                     it.Option(
-                               name="skill",
-                               description="The Leaderboard Skill",
-             		       type=it.OptionType.STRING,
-             		       required=True,
-             		       choices=[
+            description="Show Guild's Leaderboard In Total Xp Or Specific Skill",
+            options=[
+                    it.Option(
+                            name="skill",
+                            description="The Leaderboard Skill",
+             		        type=it.OptionType.STRING,
+             		        required=True,
+             		        choices=[
              			        it.Choice(name="Total",value="total"),
                 	                it.Choice(name="Combat",value="combat"),
-            	                        it.Choice(name="Mining",value="mining"),
+            	                    it.Choice(name="Mining",value="mining"),
            	                        it.Choice(name="Smithing",value="smithing"),
-             	                        it.Choice(name="Woodcutting",value="woodcutting"),
+             	                    it.Choice(name="Woodcutting",value="woodcutting"),
            	                        it.Choice(name="Crafting",value="crafting"),              
-             	                        it.Choice(name="Fishing",value="fishing"),
+             	                    it.Choice(name="Fishing",value="fishing"),
            	                        it.Choice(name="Cooking",value="cooking"),
-             	                       ],
+             	                    ],
               	              	),
-              	     it.Option(
-              	       	       name="tag",
-              	       	       description="Guild Tag To Look For",
-              	       	       type=it.OptionType.STRING,
-              	       	       required=False,
-              	       	       ),   
-                     ],		
-              )        
+              	    it.Option(
+              	       	    name="tag",
+              	       	    description="Guild Tag To Look For",
+              	       	    type=it.OptionType.STRING,
+              	       	    required=False,
+              	       	    ),   
+                    ],		
+            )        
 async def guildlb(ctx:CC,skill:str,tag:str="god"):
     await ctx.defer()
     g_tag = tag.upper()
@@ -168,7 +151,7 @@ async def guildlb(ctx:CC,skill:str,tag:str="god"):
         m_row = ActionRow(components=[pager_m])
         await ctx.edit("Finished !",embeds=[main_embed,ranking_embeds[0]],components=[m_row,b_row])
 
-  
+
 
 
 
@@ -176,82 +159,97 @@ async def guildlb(ctx:CC,skill:str,tag:str="god"):
 
 
 @bot.component("pager_menu")
-async def pager_response(ctx:it.ComponentContext,blah): 
-    chosen_page = int(ctx.data.values[0])
-    print(chosen_page)
-    data = pager_reg[str(ctx.author.user.username)] 
-    count = data[1]
-    cur_embed = data[2][chosen_page]
-    main_embed = data[3]
-    pager_reg[str(ctx.author.user.username)][0]=chosen_page
-    n_pager = pagerMaker(chosen_page,count)
-    m_row = ActionRow(components=[n_pager])
-    await ctx.edit("Finished !",embeds=[main_embed,cur_embed],components=[m_row,b_row])
+async def pager_response(ctx:CPC,blah):
+    if str(CPC.author.id) == str(CPC.message.interaction.user.id):
+        chosen_page = int(ctx.data.values[0])
+        data = pager_reg[str(ctx.author.user.username)] 
+        count = data[1]
+        cur_embed = data[2][chosen_page]
+        main_embed = data[3]
+        pager_reg[str(ctx.author.user.username)][0]=chosen_page
+        n_pager = pagerMaker(chosen_page,count)
+        m_row = ActionRow(components=[n_pager])
+        await ctx.edit("Finished !",embeds=[main_embed,cur_embed],components=[m_row,b_row])
+    else:
+        await ctx.send("You can't use this.\nRequest your own",ephemeral=True)
 
 @bot.component("first_button")
-async def first_response(ctx:it.ComponentContext):                 
-    data = pager_reg[str(ctx.author.user.username)] 
-    pager_reg[str(ctx.author.user.username)][0] = 0
-    count = data[1]
-    cur_embed = data[2][0]
-    main_embed = data[3]
-    n_pager = pagerMaker(0,count)
-    m_row = ActionRow(components=[n_pager])
-    await ctx.edit("Finished !",embeds=[main_embed,cur_embed],components=[m_row,b_row])
+async def first_response(ctx:CPC):
+    if str(CPC.author.id) == str(CPC.message.interaction.user.id):
+        data = pager_reg[str(ctx.author.user.username)] 
+        pager_reg[str(ctx.author.user.username)][0] = 0
+        count = data[1]
+        cur_embed = data[2][0]
+        main_embed = data[3]
+        n_pager = pagerMaker(0,count)
+        m_row = ActionRow(components=[n_pager])
+        await ctx.edit("Finished !",embeds=[main_embed,cur_embed],components=[m_row,b_row])
+    else:
+        await ctx.send("You can't use this.\nRequest your own",ephemeral=True)                
+
 
 @bot.component("last_button")
-async def last_response(ctx:it.ComponentContext):               
-    data = pager_reg[str(ctx.author.user.username)] 
-    chosen_page = len(data[2]) - 1
-    print(chosen_page)
-    pager_reg[str(ctx.author.user.username)][0] = chosen_page
-    count = data[1]
-    cur_embed = data[2][chosen_page]
-    main_embed = data[3]
-    n_pager = pagerMaker(chosen_page,count)
-    m_row = ActionRow(components=[n_pager])
-    await ctx.edit("Finished !",embeds=[main_embed,cur_embed],components=[m_row,b_row])
+async def last_response(ctx:CPC):  
+    if str(CPC.author.id) == str(CPC.message.interaction.user.id):
+        data = pager_reg[str(ctx.author.user.username)] 
+        chosen_page = len(data[2]) - 1
+        pager_reg[str(ctx.author.user.username)][0] = chosen_page
+        count = data[1]
+        cur_embed = data[2][chosen_page]
+        main_embed = data[3]
+        n_pager = pagerMaker(chosen_page,count)
+        m_row = ActionRow(components=[n_pager])
+        await ctx.edit("Finished !",embeds=[main_embed,cur_embed],components=[m_row,b_row])
+    else:
+        await ctx.send("You can't use this.\nRequest your own.",ephemeral=True)  
+
 
 @bot.component("backward_button")
-async def backward_response(ctx:it.ComponentContext):                 
-    data = pager_reg[str(ctx.author.user.username)] 
-    if data[0]>0:
-        chosen_page = data[0]-1
-    elif data[0] == 0:
-        chosen_page = 0
-    print(chosen_page)
-    pager_reg[str(ctx.author.user.username)][0] = chosen_page
-    count = data[1]
-    cur_embed = data[2][chosen_page]
-    main_embed = data[3]
-    n_pager = pagerMaker(chosen_page,count)
-    m_row = ActionRow(components=[n_pager])
-    await ctx.edit("Finished !",embeds=[main_embed,cur_embed],components=[m_row,b_row])
+async def backward_response(ctx:CPC):
+    if str(CPC.author.id) == str(CPC.message.interaction.user.id):                   
+        data = pager_reg[str(ctx.author.user.username)] 
+        if data[0]>0:
+            chosen_page = data[0]-1
+        elif data[0] == 0:
+            chosen_page = 0
+        pager_reg[str(ctx.author.user.username)][0] = chosen_page
+        count = data[1]
+        cur_embed = data[2][chosen_page]
+        main_embed = data[3]
+        n_pager = pagerMaker(chosen_page,count)
+        m_row = ActionRow(components=[n_pager])
+        await ctx.edit("Finished !",embeds=[main_embed,cur_embed],components=[m_row,b_row])
+    else:
+        await ctx.send("You can't use this.\nRequest your own.",ephemeral=True) 
 
 @bot.component("forward_button")
-async def forward_response(ctx:it.ComponentContext):                 
-    data = pager_reg[str(ctx.author.user.username)] 
-    if data[0]<len(data[2])-1:
-        chosen_page = data[0] + 1
-    elif data[0] == len(data[2]) - 1:
-        chosen_page = len(data[2]) - 1
-    print(chosen_page)
-    pager_reg[str(ctx.author.user.username)][0] = chosen_page
-    count = data[1]
-    cur_embed = data[2][chosen_page]
-    main_embed = data[3]
-    n_pager = pagerMaker(chosen_page,count)
-    m_row = ActionRow(components=[n_pager])
-    await ctx.edit("Finished !",embeds=[main_embed,cur_embed],components=[m_row,b_row])
+async def forward_response(ctx:CPC):
+    if str(CPC.author.id) == str(CPC.message.interaction.user.id):                 
+        data = pager_reg[str(ctx.author.user.username)] 
+        if data[0]<len(data[2])-1:
+            chosen_page = data[0] + 1
+        elif data[0] == len(data[2]) - 1:
+            chosen_page = len(data[2]) - 1
+        pager_reg[str(ctx.author.user.username)][0] = chosen_page
+        count = data[1]
+        cur_embed = data[2][chosen_page]
+        main_embed = data[3]
+        n_pager = pagerMaker(chosen_page,count)
+        m_row = ActionRow(components=[n_pager])
+        await ctx.edit("Finished !",embeds=[main_embed,cur_embed],components=[m_row,b_row])
+    else:
+        await ctx.send("You can't use this.\nRequest your own.",ephemeral=True)  
 
 @bot.component("stop_button")
-async def stop_response(ctx:it.ComponentContext):                 
-    data = pager_reg[str(ctx.author.user.username)]
-    cur_pos = data[0]
-    cur_embed = data[2][cur_pos]
-    main_embed = data[3]
-    await ctx.edit("Finished !",embeds=[main_embed,cur_embed],components=[])
-
+async def stop_response(ctx:CPC):
+    if str(CPC.author.id) == str(CPC.message.interaction.user.id):  
+        data = pager_reg[str(ctx.author.user.username)]
+        cur_pos = data[0]
+        cur_embed = data[2][cur_pos]
+        main_embed = data[3]
+        await ctx.edit("Finished !",embeds=[main_embed,cur_embed],components=[])
+    else:
+        await ctx.send("You can't use this.\nRequest your own.",ephemeral=True)
 bot.start()
 
 
