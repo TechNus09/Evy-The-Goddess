a142891fe03b31b4566900ae53809ffe0a757f99
TechNus09 <59443272+TechNus09@users.noreply.github.com>
2022-03-15 00:46:02 +0100

    ...

--- main.py
+++ main.py
@@ -134,6 +134,8 @@ async def on_ready():
 
 @bot.command(name="add_player",description="add new player to the event database",scope=922854662141526037)
 async def add_player(ctx:CC):
+    print(ctx.data)
+    print(ctx._json)
     print(dir(ctx))
     await ctx.send("ctx")
 
