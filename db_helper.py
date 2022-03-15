82a776c3e5aa135bce03072853d9e41809d10d44
TechNus09 <59443272+TechNus09@users.noreply.github.com>
2022-03-15 13:29:09 +0100

    .....

--- db_helper.py
+++ db_helper.py
@@ -53,23 +53,29 @@ def insert(t_date,e_log):
     return True
 
 def update(e_log,t_date):
-    con = conn()
-    print("con created")
-    cur = con.cursor()
-    print("cur created")
-    update_query =   """Update logs 
-                        set log = %s 
-                        where date = %s """
-    print("query created")
-    cur.execute(update_query,(e_log,t_date))
-    print("query excuted")
-    con.commit()
-    print("cur commited")
-    cur.close()
-    print("cur closed")
-    con.close()
-    print("con closed")
-    return True
+    r = False
+    try:
+        con = conn()
+        print("con created")
+        cur = con.cursor()
+        print("cur created")
+        update_query =   """Update logs 
+                            set log = %s 
+                            where date = %s """
+        print("query created")
+        cur.execute(update_query,(e_log,t_date))
+        print("query excuted")
+        con.commit()
+        print("cur commited")
+    except psycopg2.OperationalError as e:
+        print('error occured\n{0}').format(e)
+    else:
+        r = True
+        cur.close()
+        print("cur closed")
+        con.close()
+        print("con closed")
+    return r
 
 
 def retrieve(t_date):
