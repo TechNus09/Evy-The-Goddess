0b7d1f5e14217141f0b62b9f0fd1c90012b97666
TechNus09 <pegatech9@gmail.com>
2022-04-14 16:56:39 +0100

    force sync

--- main.py
+++ main.py
@@ -178,7 +178,7 @@ l_b_row = ActionRow(
 
 
 presence = it.PresenceActivity(name="Leaderboard", type=it.PresenceActivityType.WATCHING)
-bot = Client(os.getenv("TOKEN"),presence=it.ClientPresence(activities=[presence]),disable_sync=True)
+bot = Client(os.getenv("TOKEN"),presence=it.ClientPresence(activities=[presence]),disable_sync=False)
 #logging.basicConfig(level=logging.DEBUG)
 
 @bot.event
