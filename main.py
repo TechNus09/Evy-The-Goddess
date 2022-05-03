96ef362ab985a5ec006d6e35b6d129d4abdf38b9
TechNus09 <59443272+TechNus09@users.noreply.github.com>
2022-05-03 17:18:10 +0100

    enable sync

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ b_row = ActionRow(
 
 
 presence = it.PresenceActivity(name="Leaderboard", type=it.PresenceActivityType.WATCHING)
-bot = Client(os.getenv("TOKEN"),presence=it.ClientPresence(activities=[presence]),disable_sync=True)
+bot = Client(os.getenv("TOKEN"),presence=it.ClientPresence(activities=[presence]),disable_sync=False)
 #
 #logging.basicConfig(level=logging.DEBUG)
 
