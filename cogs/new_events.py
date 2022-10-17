2a8a2ddc5085d9baac0a32c080e28929cf25ab5a
TechNus09 <59443272+TechNus09@users.noreply.github.com>
2022-10-17 17:13:05 +0100

    fixinf ? take#6.5

--- cogs/new_events.py
+++ cogs/new_events.py
@@ -267,7 +267,7 @@ class Event(interactions.Extension):
         embeded_results = self.embeds_maker(xp_formatted,"OwO",skill.capitalize())
         pages = []
         for index in range(len(embeded_results[1])):
-            pages.append(embeds=[embeded_results[0],embeded_results[index]])
+            pages.append(Page(embeds=[embeded_results[0],embeded_results[index]]))
         await Paginator(client=self.client,ctx=ctx,pages=pages,).run()
 
 def setup(client:Client):
