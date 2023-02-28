bbeaa3250bff4d707dbbd7e10ac5343818de9b1a
TechNus09 <pegatech9@gmail.com>
2023-02-28 02:42:37 +0100

    test

---
+++ settings/config.py
@@ -0,0 +1,2 @@
+TOKEN = "ODgxMTc4MzEzMTYxMzEwMjI4.GyyySE.QNzdgObO-B6KXqomzipo3zb7pyUoxMmwKGNlqI"
+DB_URL = "mongodb+srv://pegatech9:pega11963112@guild.502ado8.mongodb.net/?retryWrites=true&w=majority"
